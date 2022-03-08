2e9423e60468b77db84171735933ae07d7cb0b15
Ahmed Atef <ahmedaidev@protonmail.com>
2022-03-08 19:55:17 +0200

    FIX: Add prod env variables

--- app_scuole/src/config/config.js
+++ app_scuole/src/config/config.js
@@ -1,1 +1,4 @@
-export const url = `${process.env.REACT_APP_GEOSERVER_URL}/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:scuole&outputFormat=application/json`
+const geoserver = `${process.env.REACT_APP_GEOSERVER_URL}`
+const workspace = `${process.env.REACT_APP_GEOSERVER_WORKSPACE}`
+
+export const url = `${geoserver}/geoserver/${workspace}/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=${workspace}:scuole&outputFormat=application/json`
